172ad4f26151432a7fbda0129453863f9dbb64bb
qingshi <705029004@qq.com>
2020-05-25 15:45:43 +0800

    config的git路径

---
+++ mytext/src/main/java/com.chenyilei/_6_Config_BUS.java
@@ -0,0 +1,15 @@
+package com.chenyilei;
+
+import org.springframework.cloud.bus.endpoint.RefreshBusEndpoint;
+
+/**
+ * 注释
+ * {@link org.springframework.cloud.context.config.annotation.RefreshScope}
+ * {@link RefreshBusEndpoint}
+ *
+ * @author qingshi
+ * @email 705029004@qq.com
+ * @date 2020/05/21 10:30 下午
+ */
+public class _6_Config_BUS {
+}
